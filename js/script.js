e4be00c1bc5807d5c163e9ee84b3209ba31a8513
Caio Rear <c.d.rear@gmail.com>
2023-08-27 00:03:44 +0100

    new website

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/**
+ * @copyright codewithsadee 2023
+ * @author Sadee <codewithsadee24@gmail.com>
+ */
+
+"use strict";
+
+const /** {NodeList} */ $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let /** {NodeElement} */[lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let /** {NodeElement} */[lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function () {
+
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active")
+
+        const /** {NodeElement} */ $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active"); this.classList.add("active");
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this;
+    });
+});
